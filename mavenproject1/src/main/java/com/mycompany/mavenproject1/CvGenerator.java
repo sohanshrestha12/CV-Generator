64c96161e80a06a36d4f8108819dfd239937a4c3
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-08-21 11:55:07 +0545

    initial

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/CvGenerator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Dell
+ */
+public class CvGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
